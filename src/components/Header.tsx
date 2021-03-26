0149ea9a8fbc3064027782070bc254572171e9c7
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2021-03-26 16:21:18 -0300

    miragejs, axios, react modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import logo from '../assets/logo.svg'
 import { Button, Container, Content } from '../styles/components/header';
-export function Header() {
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void
+}
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+
     return (
         <Container>
             <Content>
-            <img src={logo} alt="logo" />
-            <Button type="button">Nova transação</Button>
+                <img src={logo} alt="logo" />
+                <Button type="button" onClick={onOpenNewTransactionModal}>Nova transação</Button>
             </Content>
         </Container>
     );
